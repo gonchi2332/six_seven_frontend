498725e1056b0c84483554d18f32a9e6f80aa517
AnahiSU <anahi7776@gmail.com>
2026-04-04 16:55:48 -0400

    fix: arregle los props del boton y los estilos

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,20 @@
+import React from 'react';
+
 interface ButtonProps {
+  children: React.ReactNode; 
   variant?: 'primary' | 'secondary';
   onClick?: () => void;
   type?: 'button' | 'submit';
 }
 
 const Button = ({
+  children, 
   variant = 'primary',
   onClick, 
   type = 'button' 
 }: ButtonProps) => {
 
-  const baseStyles = "px-4 py-2 rounded-xl font-medium transition-colors text-surface font-nunito font-bold text-2xl";
+  const baseStyles = "px-6 py-2 rounded-xl transition-all duration-200 font-nunito font-bold text-2xl w-fit cursor-pointer";
 
   const variantStyles = {
     primary: "bg-secondary text-surface" ,
@@ -23,6 +27,7 @@ const Button = ({
       onClick={onClick}
       className={`${baseStyles} ${variantStyles[variant]}`}
     >
+      {children} 
     </button>
   );
 };
