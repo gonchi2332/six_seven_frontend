e4b274f2950e403abfab86a5040a48a34cebc8e9
Ignacio Jaldin <ignaciojaldin72@gmail.com>
2026-04-04 12:28:37 -0400

    feat: Agregar tailwind al proyecto

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+    return (
+        <div className="min-h-screen bg-blue-500 flex items-center justify-center">
+            <h1 className="text-4xl font-bold text-white">Tailwind funciona!</h1>
+        </div>
+    )
+}
+export default App
---
+++ src/vite-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="vite/client" />
+declare module "*.css" {
+    const content: any;
+    export default content;
+}
