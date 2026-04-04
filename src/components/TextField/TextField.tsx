f60c2cdc557469b2fa18f807e9560ffcde4950b8
AnahiSU <anahi7776@gmail.com>
2026-04-04 17:54:38 -0400

    fix: value ya no es obligatorio

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 interface TextFieldProps {
   label?: string;
-  value: string;
+  value?: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   placeholder?: string;
   type?: 'text' | 'password' | 'email' | 'number'; 
