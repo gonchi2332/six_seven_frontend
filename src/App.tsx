61cdb1c847bc064dfec6616cda82d9403a54a8d8
Sergio Maldonado <sergiomaldonadosejas@gmail.com>
2026-04-06 19:46:38 -0400

    creacion de registro para los datos de los nuevos usuarios

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import RegisterPage from "./pages/registerPage"
 function App() {
     return (
-        <div className="min-h-screen bg-blue-500 flex items-center justify-center">
-            <h1 className="text-4xl font-bold text-white">Tailwind funciona!</h1>
+        <div className="App">
+            <RegisterPage />
         </div>
     )
 }
