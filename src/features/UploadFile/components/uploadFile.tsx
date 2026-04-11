14072af00598a6236b6a0db6f16ad141f422712d
AnahiSU <anahi7776@gmail.com>
2026-04-11 11:15:41 -0400

    fix: se anadio un salto de linea para especificar formatos de archivo

--- src/features/UploadFile/components/uploadFile.tsx
+++ src/features/UploadFile/components/uploadFile.tsx
@@ -58,7 +58,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({ onImageSelect }) => {
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path>
             </svg>
             <p className="mb-1 text-sm text-surface">
-              <span className="font-semibold text-primary">Subir foto</span>
+              <span className="font-semibold text-primary">Subir foto<br/>(png, jpeg o jpg)</span>
             </p>
           </div>
         )}
