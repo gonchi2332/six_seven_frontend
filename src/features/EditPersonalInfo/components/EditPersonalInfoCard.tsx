c270f231575a8f2b2ff4be14577eebabb98dbca8
AnahiSU <anahi7776@gmail.com>
2026-04-11 11:14:58 -0400

    refactor: el hook se movio con los tipados, se anadio regex

--- src/features/EditPersonalInfo/components/EditPersonalInfoCard.tsx
+++ src/features/EditPersonalInfo/components/EditPersonalInfoCard.tsx
@@ -1,67 +1,47 @@
 import TextField from "../../../components/TextField";
 import Button from "../../../components/Button";
 import PopUpCard from "../../../components/PopUpCard";
-import { useState } from "react";
 import ImageUpload from "../../UploadFile/components/uploadFile";
-
-interface FormData {
-  firstName: string;
-  lastNamePaternal: string;
-  lastNameMaternal: string;
-  address: string;
-  email: string;
-  phone: string;
-  country: string;
-  profileImage: File | null; 
-}
+import { useCountries } from "../../../hooks/useCountries";
+import { useProfileForm } from "../../../hooks/useProfileFormRegex"; 
 
 const EditPersonalInfoCard = () => {
-  const [formData, setFormData] = useState<FormData>({
-    firstName: "",
-    lastNamePaternal: "",
-    lastNameMaternal: "",
-    address: "",
-    email: "",
-    phone: "",
-    country: "",
-    profileImage: null, 
-  });
-
-  const handleChange = (field: keyof FormData, value: string | File | null) => {
-    setFormData((prev) => ({
-      ...prev,
-      [field]: value,
-    }));
-  };
+  const { countries, isLoading } = useCountries();
+  const { formData, errors, handleChange, validateForm } = useProfileForm();
 
   const handleAcept = () => {
+    if (!validateForm()) {
+      // si existen errores
+      return;
+    }
     // apartado para backend
   };
-
   const handleCancel = () => {
     // apartado para backend
   };
 
   return (
     <div>
-      <PopUpCard title={"Editar Perfil"}>
+      <PopUpCard title={"Datos Personales"}>
         <div>
           <div className="flex flex-col gap-6 px-8">
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
               <TextField
-                label="Nombres:"
+                label="Nombre(s)*:"
                 value={formData.firstName}
                 onChange={(e) => handleChange("firstName", e.target.value)}
                 placeholder="Ej: Juan Dominic"
+                error={errors.firstName}
                 className="w-full"
               />
               <TextField
-                label="Apellido Paterno:"
+                label="Apellido Paterno*:"
                 onChange={(e) =>
                   handleChange("lastNamePaternal", e.target.value)
                 }
                 value={formData.lastNamePaternal}
                 placeholder="Ej: Pérez"
+                error={errors.lastNamePaternal}
                 className="w-full"
               />
               <TextField
@@ -71,6 +51,7 @@ const EditPersonalInfoCard = () => {
                   handleChange("lastNameMaternal", e.target.value)
                 }
                 placeholder="Ej: Sanchez"
+                error={errors.lastNameMaternal}
                 className="w-full"
               />
             </div>
@@ -89,26 +70,43 @@ const EditPersonalInfoCard = () => {
                 onChange={(e) => handleChange("email", e.target.value)}
                 placeholder="Ej: juan@ejemplo.com"
                 type="email"
+                error={errors.email} 
                 className="w-full"
               />
               <TextField
                 label="Teléfono:"
                 value={formData.phone}
                 onChange={(e) => handleChange("phone", e.target.value)}
-                placeholder="Ej: 77123456"
+                placeholder="Ej: +591 77123456" 
                 type="text"
+                error={errors.phone}
                 className="w-full"
               />
             </div>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-              <TextField
-                label="País de residencia:"
-                value={formData.country}
-                onChange={(e) => handleChange("country", e.target.value)}
-                placeholder="Ej: Bolivia"
-                className="w-full"
-              />
-              {/* Aquí integramos el componente pasándole el callback */}
+              <div className="flex flex-col justify-start">
+                <label className="mb-1 text-xl font-inter text-white">
+                  País de residencia:
+                </label>
+                <select
+                  value={formData.country}
+                  onChange={(e) => handleChange("country", e.target.value)}
+                  disabled={isLoading}
+                  className={`w-full px-4 py-2 border rounded-xl outline-none transition-all duration-200 bg-white font-nunito disabled:cursor-not-allowed border-gray-300 focus:border-blue-500 focus:ring-1 focus:ring-blue-500 ${
+                    formData.country === "" ? "text-gray-400" : "text-black"
+                  }`}
+                >
+                  <option value="" disabled className="text-gray-400 hidden">
+                    {isLoading ? "Cargando países..." : "Ej: Bolivia"}
+                  </option>
+                  {countries.map((countryName, index) => (
+                    <option key={index} value={countryName} className="text-black">
+                      {countryName}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
               <div className="flex flex-col gap-2">
                 <span className="mb-1 text-xl font-inter text-white">Imagen de perfil:</span>
                 <ImageUpload 
