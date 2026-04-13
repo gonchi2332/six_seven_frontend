ee0553905fe5d7d384df85102001789d1790cc17
AnahiSU <anahi7776@gmail.com>
2026-04-12 23:03:27 -0400

    feat: anadi un nuevo componente de navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar = () =>{
+    return(
+        //TODO: cambiar el nombre estatico por el que nos daran en los endpoints
+        //TODO: cambiar la imagen estatica por la que nos daran en los endpoints
+        <nav className="w-full bg-secondary p-4 text-white font-inter font-bold text-xl">
+            <div>
+                <img src="https://www.google.com/url?sa=t&source=web&rct=j&url=https%3A%2F%2Fwww.vecteezy.com%2Ffree-vector%2Fplaceholder&ved=0CBcQjRxqFwoTCOiGpITj6ZMDFQAAAAAdAAAAABAI&opi=89978449" alt="" />
+                Ignacio Jaldin Janko
+            </div>
+        </nav>
+    );
+}
+export default Navbar;
