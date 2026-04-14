cfca9be00d9e42b56e2e9ab694bfb70e65a19982
AnahiSU <anahi7776@gmail.com>
2026-04-14 01:50:24 -0400

    fix: quitado del navbar

--- src/pages/VerificationPage.tsx
+++ src/pages/VerificationPage.tsx
@@ -1,10 +1,8 @@
-import Navbar from "../components/Navbar/Navbar";
 import EmailVerificationFlow from "../features/auth/components/EmailPasswordVerification/EmailVerificationFlow";
 
 const VerificationPage = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar />
       <div className="flex justify-center items-center flex-1">
         <EmailVerificationFlow initialMode="verify" />
       </div>
