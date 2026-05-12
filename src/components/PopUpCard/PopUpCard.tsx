93e1316c9e68b730b6e683dc34a4766d7e197ccc
Ignacio Jaldin <ignaciojaldin72@gmail.com>
2026-05-11 23:05:03 -0400

    fix: modal con hu

--- src/components/PopUpCard/PopUpCard.tsx
+++ src/components/PopUpCard/PopUpCard.tsx
@@ -1,21 +1,21 @@
 import type React from "react";
 
 interface PopUpCardProps {
-  title: string;
-  children?: React.ReactNode;
+    title: string;
+    children?: React.ReactNode;
 }
 
 const PopUpCard = ({ title, children }: PopUpCardProps) => {
-  return (
-    <div className="bg-primary rounded-xl pb-4 w-full max-w-5xl mx-auto shadow-2xl">
-      <div className="mb-8 border-b-2 border-surface pb-4 w-full">
-        <h2 className="text-3xl font-bold text-surface text-center font-inter align-center pt-4">
-          {title}
-        </h2>
-      </div>
-      <div className="w-full">{children}</div>
-    </div>
-  );
+    return (
+        <div className="bg-primary rounded-xl pb-4 w-full max-w-5xl mx-auto shadow-2xl">
+            <div className="mb-8 border-b-2 border-surface pb-4 w-full">
+                <h2 className="text-3xl font-bold text-surface text-center font-inter align-center pt-4 px-6">
+                    {title}
+                </h2>
+            </div>
+            <div className="w-full">{children}</div>
+        </div>
+    );
 };
 
 export default PopUpCard;
